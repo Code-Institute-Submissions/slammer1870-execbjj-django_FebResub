2aa569c565c96d3a7cc7b0fda6b1969695dc108d
Sam <hello@sammcnally.dev>
2021-10-14 10:29:31 +0100

    fix: textarea

--- members/forms.py
+++ members/forms.py
@@ -17,4 +17,4 @@ class NewsletterForm(forms.Form):
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     email = forms.EmailField(label='Email', max_length=100)
-    message = forms.CharField(label='Message', max_length=250)
+    message = forms.CharField(label='Message', max_length=250, widget=forms.Textarea(attrs={'rows':5, 'cols':10}))
