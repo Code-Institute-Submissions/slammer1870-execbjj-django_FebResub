2be52186d36b3b9fc2edd3bf1ae6ac3bb4560e8e
Sam <hello@sammcnally.dev>
2022-01-06 16:33:39 +0000

    fix: attempt to fix videos

--- videos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Video, Section, Difficulty
-
-# Register your models here.
-admin.site.register(Video)
-admin.site.register(Section)
-admin.site.register(Difficulty)
